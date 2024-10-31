97681a196c393ad44c459b434d60d2bb5360c93b
jzn <waltiu@163.com>
2024-10-31 11:16:39 +0800

    feat: 新增框架和包管理工具选择

---
+++ constant/framework.js
@@ -0,0 +1,16 @@
+const FRAMEWORK_VUE2 = "vue2";
+const FRAMEWORK_VUE3 = "vue3";
+const FRAMEWORK_REACT = "react";
+
+const frameworkList = [
+  {
+    value: FRAMEWORK_VUE2,
+  },
+  {
+    value: FRAMEWORK_VUE3,
+  },
+  {
+    value: FRAMEWORK_REACT,
+  },
+];
+export default frameworkList;
---
+++ constant/packageManager.js
@@ -0,0 +1,16 @@
+const PACKAGE_MANAGER_NPM = "npm";
+const PACKAGE_MANAGER_YARN = "yarn";
+const PACKAGE_MANAGER_PNPM = "pnpm";
+
+const packageManagerList = [
+  {
+    value: PACKAGE_MANAGER_NPM,
+  },
+  {
+    value: PACKAGE_MANAGER_YARN,
+  },
+  {
+    value: PACKAGE_MANAGER_PNPM,
+  },
+];
+export default packageManagerList;
